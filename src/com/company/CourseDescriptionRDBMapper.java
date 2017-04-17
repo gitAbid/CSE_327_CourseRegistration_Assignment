c99dfaae6eee1d8d5bccebebe43ebe263377e34c
Abid Hasan <abid.northsouth@gmail.com>
2017-04-17 11:14:40 +0600

    Revision 5
    Added and Implemented Template Pattern

--- src/com/company/CourseDescriptionRDBMapper.java
+++ src/com/company/CourseDescriptionRDBMapper.java
@@ -6,15 +6,19 @@ import java.sql.SQLException;
 /**
  * Created by Abid Hasan on 3/24/2017.
  */
-public class CourseDescriptionRDBMapper implements IMapper {
+public class CourseDescriptionRDBMapper extends RDBMapper {
 
     private DatabaseConnector DBconnector;
 
     public CourseDescriptionRDBMapper() {
+        super("courses");
         DBconnector=new DatabaseConnector();
     }
 
     @Override
+<<<<<<< HEAD
+
+=======
     public Object get(String oid) {
         String query="SELECT * FROM courses WHERE course_id='"+oid.trim().toUpperCase()+"'";
 
@@ -36,9 +40,31 @@ public class CourseDescriptionRDBMapper implements IMapper {
     }
 
     @Override
+>>>>>>> origin/master
     public void put(Object object) {
         Course course = (Course) object;
         String query="INSERT INTO courses (course_id,course_name,course_credit,credit_tution) VALUES ('"+course.getId().trim().toUpperCase()+"','"+course.getTitle().trim()+"',"+course.getCredit()+","+course.getTuitionPerCredit()+")";
         DBconnector.addData(query);
     }
+
+    @Override
+    protected Object getObjectFromRecord(String oid, Object dbRecord) {
+
+
+        ResultSet resultSet =(ResultSet) dbRecord;
+        Course ResultCourse = new Course();
+        try {
+            while (resultSet.next()){
+                ResultCourse.setId(resultSet.getString("course_id"));
+                ResultCourse.setTitle(resultSet.getString("course_name"));
+                ResultCourse.setCredit(resultSet.getInt("course_credit"));
+                ResultCourse.setTuitionPerCredit(resultSet.getInt("credit_tution"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ResultCourse;
+    }
+
 }
